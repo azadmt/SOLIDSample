de018ee1a4011d588a49fc053b3ba8d157ea8afe
mohammad azad <EsmaeilAzad.Mohamad@Golrang.com>
2020-08-22 19:46:46 +0430

    remove refactored file

--- Logger/Program.cs
+++ Logger/Program.cs
@@ -1,4 +1,4 @@
-﻿using Logger.Refactor;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,21 +12,6 @@ namespace Logger
         static void Main(string[] args)
         {
            
-        }
-
-        static void FactoryStrategySample()
-        {
-            var loggerType = LoggerFactory.CreateLogger("TEXTFILE");
-            var logger = new LoggerContext();
-            logger.SetLogger(loggerType);
-            logger.Log("Logg ");
-        }
-
-        static void StrategySample()
-        {
-            var logger = new LoggerContext();
-            logger.SetLogger(new FileLogger());
-            logger.Log("Logg ");
         }
     }
 }
